a7e65fe54d142af8980744b697e1f425df2a42f1
Luke <LukeHenderson817@yahoo.com>
2020-03-13 19:21:38 -0500

    waiting for key enable

---
+++ notify.py
@@ -0,0 +1,54 @@
+#https://realpython.com/python-send-email/
+#https://developers.upwork.com/?lang=python#introduction
+
+
+
+
+# #not working code
+# import smtplib, ssl
+#
+# port = 465  # For SSL
+# smtp_server = "smtp.gmail.com"
+# sender_email = "WebCrawler1996@gmail.com"  # Enter your address
+# receiver_email = "12145491447@txt.att.net"  # Enter receiver address
+# password = "jhUjhUjhU$"
+# #input("Type your password and press enter: ")
+# message = """\
+# Subject: Hi there
+#
+# This message is sent from Python."""
+#
+# context = ssl.create_default_context()
+# with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+#     server.login(sender_email, password)
+#     server.sendmail(sender_email, receiver_email, message)
+
+
+#numbers
+#2145491447@txt.att.net
+#2142286829@txt.att.net
+#9724151565
+
+
+
+
+
+#working code
+import smtplib, ssl
+
+port = 587  # For starttls
+smtp_server = "smtp.gmail.com"
+sender_email = "WebCrawler1996@gmail.com"
+receiver_email = "9724151565@txt.att.net"
+password = "jhUjhUjhU$"
+#input("Type your password and press enter: ")
+message = """\
+Subject: HI BUBBY."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP(smtp_server, port) as server:
+    server.ehlo()  # Can be omitted
+    server.starttls(context=context)
+    server.ehlo()  # Can be omitted
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
--- ping.py
+++ ping.py
@@ -1,24 +1,40 @@
 import requests
-
-url = "https://www.upwork.com/api/profiles/v2/search/jobs.json"
-
-response = requests.request("GET", url)
-
-print(response.text)
-
-
-
-sleep(10000)
-
-
-
-#example code
-
-client.provider_v2.search_jobs(
-    data={PARAMS_DICT}, page_offset=0, page_size=20)
-
-#EXAMPLE REQUEST:
+from time import sleep
 import upwork
-client = upwork.Client(public_key, secret_key, **credentials)
-data = {'q': 'python', 'title': 'Web developer'}
-client.provider_v2.search_jobs(data=data)
+
+
+# # url = "https://www.upwork.com/api/profiles/v2/search/jobs.json"
+# # POST /api/auth/v1/oauth/token/request
+# url = "https://www.upwork.com/api/auth/v1/oauth/token/request"
+#
+# # response = requests.request("GET", url)
+# response = requests.request("POST", url)
+#
+# print(response.text)
+
+
+public_key = "6cc64b61b06a6d9446d17daec5aef053"
+secret_key = "b923e71b020a22ab"
+credentials = {}
+#Instantiating a client without an auth token
+client = upwork.Client(public_key, secret_key)
+# client = upwork.Client(public_key, secret_key, **credentials)
+# client.auth.get_authorize_url()
+client.auth.get_request_token()
+
+
+
+# sleep(10000)
+
+
+
+# #example code
+#
+# client.provider_v2.search_jobs(
+#     data={PARAMS_DICT}, page_offset=0, page_size=20)
+#
+# #EXAMPLE REQUEST:
+# import upwork
+# client = upwork.Client(public_key, secret_key, **credentials)
+# data = {'q': 'python', 'title': 'Web developer'}
+# client.provider_v2.search_jobs(data=data)
